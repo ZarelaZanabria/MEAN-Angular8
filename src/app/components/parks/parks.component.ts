051081357f5a4ef0cf4b5ee1ee74fe7819529979
ZarelaZanabria <zarelixzana@gmail.com>
2019-10-05 17:13:03 -0500

    Aprendiendosobre sobre el cliclo de vida de un componente

--- src/app/components/parks/parks.component.ts
+++ src/app/components/parks/parks.component.ts
@@ -1,31 +1,49 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges, DoCheck, OnDestroy } from '@angular/core';
 
 
+// tslint:disable-next-line:no-conflicting-lifecycle
 @Component({
   selector: 'app-parks',
   templateUrl: './parks.component.html',
   styleUrls: ['./parks.component.css']
 })
-export class ParksComponent implements OnInit {
+export class ParksComponent implements OnInit, OnChanges , DoCheck , OnDestroy{
 
-  @Input() name : string; 
-  public metro : number;
-  public vegetacion : string;
-  public abierto : boolean;
+  @Input() name: string;
+  public metro: number;
+  public vegetacion: string;
+  public abierto: boolean;
 
   @Output () pasandoDatos = new  EventEmitter();
 
-  constructor() { 
-    this.name = "The park zoolo"
+  constructor() {
+    this.name = 'The park zoolo';
     this.metro = 40;
     this.vegetacion = 'Alta';
     this.abierto = false;
   }
 
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngOnChanges(changes: SimpleChanges ): void {
+    console.log('Cambios', changes);
+
+
+  }
   ngOnInit() {
+    console.log('metodo Oninit');
   }
 
-  sendInformation(){
+  ngDoCheck(): void {
+    console.log('El Docheck se ha ejecutado');
+
+  }
+  ngOnDestroy(): void {
+    console.log('Cuando se elimina ');
+
+  }
+
+  sendInformation() {
     this.pasandoDatos.emit({
      name : this.name,
      metro: this.metro,
--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -1,28 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
-@Component({ //Decorador 
-  selector: 'app-store', //Matadatos en que etiqueta 
+@Component({ // Decorador
+  selector: 'app-store', // Matadatos en que etiqueta
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
-  public title : string;
-  public list : string;
+  public title: string;
+  public list: string;
 
-  public titlePark : string; 
+  public titlePark: string;
+  public parkObject: any;
 
-  constructor() { 
+  constructor() {
     this.title = 'Componente tienda';
     this.list = 'Bonca Saga';
+
 
   }
 
   ngOnInit() {
   }
 
-  mostrar(){
+  mostrar() {
     console.log(this.titlePark);
-    
+
+  }
+  public verDatosPark(event) {
+    console.log(event);
+    this.parkObject = event;
+
   }
 
 }
